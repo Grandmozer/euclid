26893a43b671f1a87616059eb07a82922f6ad534
Grandmozer <svetoskover@gmail.com>
2023-07-03 14:48:34 +0500

    input for mobile screen

--- Euclid/JS/search.js
+++ Euclid/JS/search.js
@@ -3,7 +3,9 @@ const inputSearch = document.getElementById("input-search");
     closeSearch = document.getElementById("close-search");
     btnSearch = document.getElementById("btn-search");
     formSearch = document.getElementById("form-search");
-    information = document.querySelectorAll('.s');
+    burger = document.getElementById("burger");
+    logoHeader = document.getElementById("logoHeader");
+    // information = document.querySelectorAll('.s');
 
 btnSearch.addEventListener('click', () => {
   inputSearch.classList.add('input-search-active');
@@ -12,6 +14,13 @@ btnSearch.addEventListener('click', () => {
   formSearch.classList.add('search-active');
   closeSearch.classList.add('btn-close-search-active');
   // FindOnPage('text-to-find');
+
+  if(window.matchMedia("(max-width: 768px)").matches){
+    logoHeader.classList.add('do-not-display');
+  }  
+  if(window.matchMedia("(max-width: 550px)").matches){
+    burger.classList.add('do-not-display');
+  }
 });
 closeSearch.addEventListener('click', () => {
     inputSearch.classList.remove('input-search-active');
@@ -19,6 +28,8 @@ closeSearch.addEventListener('click', () => {
     formSearch.classList.add('search-off');    
     formSearch.classList.remove('search-active');
     closeSearch.classList.remove('btn-close-search-active');
+    logoHeader.classList.remove('do-not-display');
+    burger.classList.remove('do-not-display');
     
     inputSearch.value = '';
 });
