b73aacace2eb2ee7d08cd3dbbbc76f30bd8a0a8a
Grandmozer <svetoskover@gmail.com>
2023-07-28 19:24:59 +0500

    end

--- Euclid/JS/burger.js
+++ Euclid/JS/burger.js
@@ -1,7 +1,15 @@
-window.addEventListener('DOMContentLoaded',function(){
-    document.getElementById('burger').addEventListener('click', function(){
-        document.getElementById('menu').classList.toggle('is-active');
-        console.log(document.getElementById('menu'))
-    });
+// window.addEventListener('DOMContentLoaded',function(){
+//     document.getElementById('burger').addEventListener('click', function(){
+//         document.getElementById('menu').classList.toggle('is-active');
+//         console.log(document.getElementById('menu'))
+//     });
   
-}) 
+// }) 
+
+let menuBtn = document.querySelector('.menu-btn');
+let menu = document.querySelector('.menu');
+
+menuBtn.addEventListener('click', function(){
+	menuBtn.classList.toggle('active');
+	menu.classList.toggle('active');
+})
--- Euclid/JS/swiper.js
+++ Euclid/JS/swiper.js
@@ -4,10 +4,9 @@ const two = document.getElementById('stepTwo');
 const three = document.getElementById('stepThree');
 const four = document.getElementById('stepFour');
 
-const title = document.getElementById('titleHowWeWork');
-const text = document.getElementById('textHowWeWork');
-
-const img = document.getElementById('imgHowWork');
+window.addEventListener('load', () => {
+    one.classList.add('step-active');
+  });
 
 one.addEventListener('click',() => {
     one.classList.add('step-active');
@@ -15,8 +14,25 @@ one.addEventListener('click',() => {
     two.classList.remove('step-active');
     three.classList.remove('step-active');
     four.classList.remove('step-active');
+});
+two.addEventListener('click',() => {
+    two.classList.add('step-active');
 
-    title.textContent = "Проводим консультацию";
-    text.textContent = `Влечёт за собой процесс внедрения и модернизации приоритизации разума над эмоциями. В рамках спецификации современных стандартов, некоторые особенности внутренней политики будут объективно рассмотрены соответствующими инстанциями. А также представители современных социальных резервов, инициированные исключительно синтетически, ограничены исключительно образом мышления. Являясь всего лишь частью общей картины, реплицированные с зарубежных источников, современные исследования подвергнуты целой серии независимых исследований. Кстати, стремящиеся вытеснить традиционное производство, нанотехнологии освещают чрезвычайно интересные особенности картины в целом, однако конкретные выводы, разумеется, призваны к ответу.`
- 
-})
+    one.classList.remove('step-active');
+    three.classList.remove('step-active');
+    four.classList.remove('step-active');
+});
+three.addEventListener('click',() => {
+    three.classList.add('step-active');
+
+    two.classList.remove('step-active');
+    one.classList.remove('step-active');
+    four.classList.remove('step-active');
+});
+four.addEventListener('click',() => {
+    four.classList.add('step-active');
+
+    two.classList.remove('step-active');
+    three.classList.remove('step-active');
+    one.classList.remove('step-active');
+});
